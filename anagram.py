48e8e457852e7eefe85cd77dd37ad367b049b6e3
mrKYLN <mertkayalan@gmail.com>
2020-10-03 18:29:40 +0300

    second commit

--- anagram.py
+++ anagram.py
@@ -20,4 +20,7 @@ def anagram_counter(words):
         
         return result                
                 
-anagram_counter(['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab'])
+Test.assert_equals(anagram_counter([]), 0, "Is not returning a number")
+Test.assert_equals(anagram_counter(['dell', 'ledl', 'abc', 'cba']), 2)
+Test.assert_equals(anagram_counter(['dell', 'ledl', 'lled', 'cba']), 3)
+Test.assert_equals(anagram_counter(['dell', 'ledl', 'abc', 'cba', 'bca', 'bac', 'cab']), 11)
